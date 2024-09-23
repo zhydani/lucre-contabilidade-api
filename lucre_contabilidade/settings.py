e0b4420f7a6495794012f5bcb29668355233c0a5
zhydani <zhydani@gmail.com>
2024-09-23 15:55:10 -0300

    media

--- lucre_contabilidade/settings.py
+++ lucre_contabilidade/settings.py
@@ -5,6 +5,10 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
 
 DATABASES = {
     'default': dj_database_url.config(
